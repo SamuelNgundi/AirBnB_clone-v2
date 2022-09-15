36249522c08fd96b82cf1d99614f11e1e4d4b1e3
Je4stine <je4stine@gmail.com>
2022-09-15 11:20:25 +0300

    Corrections

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-Your web application must be listening on 0.0.0.0, port 5000
-You must use the option strict_slashes=False in your route definition
-/: display 'Hello HBNB!'
-/hbnb: display 'HBNB'
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
 """
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_world():
-    """
-    returns the message to display in our browser
-    """
-    return 'Hello HBNB!'
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    display "HBNB" in the browser
-    """
-    return 'HBNB'
+    """prints HBNB"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def Cisfun(text):
-    """
-    display "C" followed by the value of the text variable
-    """
-    string = 'C %s' % text
-    return string.replace('_', ' ')
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
-@app.route('/python/<text>', strict_slashes=False)
-def Pytho(text):
-    """
-    display "Python", followed by the value of text variable
-    """
-    string = 'Python %s' % text
-    return string.replace('_', ' ')
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """prints Python is cool"""
+    text = text.replace("_", " ")
+    return "Python %s" % text
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,74 +1,63 @@
 #!/usr/bin/python3
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
+    /number/<n> - display n if integer
+    /number_template/<n> - display a HTML page if n is int
+    /number_odd_or_even/<n> - displays if a number is odd or even
 """
-starts a Flask web application
-Your web application must be listening on 0.0.0.0, port 5000
-You must use the option strict_slashes=False in your route definition
-/: display 'Hello HBNB!'
-/hbnb: display 'HBNB'
-"""
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_world():
-    """
-    returns the message to display in our browser
-    """
-    return 'Hello HBNB!'
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    display "HBNB" in the browser
-    """
-    return 'HBNB'
+    """prints HBNB"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def Cisfun(text):
-    """
-    display "C" followed by the value of the text variable
-    """
-    string = 'C %s' % text
-    return string.replace('_', ' ')
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
-@app.route('/python/<text>', strict_slashes=False)
-def Pytho(text):
-    """
-    display "Python", followed by the value of text variable
-    """
-    string = 'Python %s' % text
-    return string.replace('_', ' ')
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """prints Python is cool"""
+    text = text.replace("_", " ")
+    return "Python %s" % text
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def is_number(n):
-    """
-    display "n is a number" only if n is an integer
-    """
-    return "%d is a number" % n
+def number_n(n):
+    """displays n if integer"""
+    return "%i is a number" % n
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """
-    display HTML
-    """
+    """display a HTML page if n is int"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def odd_or_even(n):
-    """
-    display a HTML page only if n is integer
-    H1 tag: 'Number: n is even|odd' inside the tag BODY
-    """
+def number_odd_or_even(n):
+    """display a HTML page if n is int"""
     return render_template('6-number_odd_or_even.html', n=n)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
