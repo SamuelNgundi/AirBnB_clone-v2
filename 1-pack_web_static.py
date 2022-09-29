70dc81943b923db2259792a05cf0e95a909655d8
SamuelNgundi <samuelwngundi02@gmail.com>
2022-09-29 18:46:44 +0300

    Compresss

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Fabric script to generate .tgz archive'''
+'''Fabric script to generate .tgz archive for server'''
 
 from fabric.api import local
 from datetime import datetime
